33c04dc5d532388d69d587c5192f7bc114af38fc
MankiniChykan <101962955+MankiniChykan@users.noreply.github.com>
2025-11-11 10:15:56 +0000

    modified:   rollup.config.js

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,7 @@
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import replace from '@rollup/plugin-replace';
-import { terser } from '@rollup/plugin-terser';
+import terser from '@rollup/plugin-terser';
 import typescript from 'rollup-plugin-typescript2';
 
 const production = !process.env.ROLLUP_WATCH;
@@ -12,19 +12,19 @@ export default {
     dir: 'dist',
     format: 'es',
     sourcemap: true,
-    entryFileNames: 'nabu-eyes-dashboard-card.js'
+    entryFileNames: 'nabu-eyes-dashboard-card.js',
   },
   plugins: [
     replace({
       preventAssignment: true,
-      'process.env.NODE_ENV': JSON.stringify(production ? 'production' : 'development')
+      'process.env.NODE_ENV': JSON.stringify(production ? 'production' : 'development'),
     }),
     resolve(),
     commonjs(),
     typescript({
       tsconfig: './tsconfig.json',
-      useTsconfigDeclarationDir: true
+      useTsconfigDeclarationDir: true,
     }),
-    production && terser()
-  ]
+    production && terser(),
+  ],
 };
