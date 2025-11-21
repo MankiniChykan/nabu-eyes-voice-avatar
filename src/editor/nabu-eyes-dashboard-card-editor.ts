aa594abc6065369f81b4167dbc1183734c731644
MankiniChykan <101962955+MankiniChykan@users.noreply.github.com>
2025-11-21 01:29:06 +0000

    modified:   src/editor/nabu-eyes-dashboard-card-editor.ts

--- src/editor/nabu-eyes-dashboard-card-editor.ts
+++ src/editor/nabu-eyes-dashboard-card-editor.ts
@@ -39,9 +39,10 @@ const idleVariantOptions = Object.entries(STATE_VARIANTS.idle).map(([value, labe
   label,
 }));
 
-const listeningVariantOptions = Object.entries(STATE_VARIANTS.listening).map(
-  ([value, label]) => ({ value, label }),
-);
+const listeningVariantOptions = Object.entries(STATE_VARIANTS.listening).map(([value, label]) => ({
+  value,
+  label,
+}));
 
 const processingVariantOptions = Object.entries(STATE_VARIANTS.processing).map(
   ([value, label]) => ({ value, label }),
@@ -56,9 +57,10 @@ const alarmVariantOptions = Object.entries(STATE_VARIANTS.alarm).map(([value, la
   label,
 }));
 
-const countdownVariantOptions = Object.entries(STATE_VARIANTS.countdown).map(
-  ([value, label]) => ({ value, label }),
-);
+const countdownVariantOptions = Object.entries(STATE_VARIANTS.countdown).map(([value, label]) => ({
+  value,
+  label,
+}));
 
 const muteVariantOptions = Object.entries(STATE_VARIANTS.mute).map(([value, label]) => ({
   value,
